aa2dd54ff3b8d6fddf794a5df5f3571d91374299
Yusuf Çağlar <faf7faf7@gmail.com>
2022-02-12 19:44:49 +0300

    Initial Commit

---
+++ In-App Purchase Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  In-App Purchase Demo
+//
+//  Created by Yusuf Çağlar on 12.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ In-App Purchase Demo/In_App_Purchase_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_App_Purchase_DemoApp.swift
+//  In-App Purchase Demo
+//
+//  Created by Yusuf Çağlar on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct In_App_Purchase_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
